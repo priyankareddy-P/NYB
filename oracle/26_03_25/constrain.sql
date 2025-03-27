d3d374b10b9575cee70c149e1c2cef7eb55223cd
priyanka <priyanka.elluri@navayuvabharatinfotech.com>
2025-03-27 11:20:12 +0530

    constrain

---
+++ oracle/26_03_25/constrain.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students (
+   student_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+   age INT
+ );
+ 
+ 
+ 
+ --Insert the data
+ INSERT INTO students (student_id, name, email, age)
+ values(1, 'priya', 'priyanka32gmail.com', 20);
+ values(2,'hima','hima2@gmail.com',30);
