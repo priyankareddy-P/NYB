f895af43cf6288d5a9e27e04afa4e082cb60944a
priyanka <priyanka.elluri@navayuvabharatinfotech.com>
2025-04-01 17:23:11 +0530

    functions

---
+++ javascript/25_03_25/functions.js
@@ -0,0 +1,42 @@
+var details = {
+    name: 'priya',
+    number: 1223344,
+    age: 23
+}
+console.log(details)
+
+
+//functions
+console.log("functions")
+function hello() {
+    //code
+    var a = 10
+    var b = 10
+    console.log(a)
+    console.log(b)
+    return "hello this is a function"
+
+
+}
+
+
+
+
+hello()   //function calling
+
+
+
+
+
+
+
+
+
+
+
+var val = hello ();
+console.log(val)
+console.log(hello())
+hello()
+hello()
+
