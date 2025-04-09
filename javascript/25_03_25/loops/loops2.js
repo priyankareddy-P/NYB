1de7c854925921f6a82216eb5a0437f555903c40
priyanka <priyanka.elluri@navayuvabharatinfotech.com>
2025-04-09 17:57:17 +0530

    loops

---
+++ javascript/25_03_25/loops/loops2.js
@@ -0,0 +1,88 @@
+let marks = 90;
+if(marks >= 60) {
+    console.log("You passed");
+}
+else {
+    console.log("You failed");
+}
+
+
+// for loop
+
+for (let i = 1; i <= 5; i++)
+{
+    console.log("Hello");
+}
+
+//print numbers from 1 to 5
+
+for (let i = 1; i <= 5; i++)
+{
+    console.log(i);
+}
+
+//print even numbers from 2 to 10
+
+for (let i = 2; i <= 10; i +=2)
+{
+    console.log(i);
+}
+
+//print the square of numbers from 1 to 20
+
+for (let i = 2; i <= 20; i++)
+{
+    console.log("square of ", i, "is", i*i);
+}
+
+
+
+//print the cube of numbers from 1 to 10
+
+for (let i = 1; i <= 10; i++)
+{
+    console.log("cube of", i,"is", i*i*i);
+}
+
+
+//while loop
+
+let i = 1;
+while (i <= 5)
+{
+    console.log(i);
+    i++;
+}
+
+//print odd numbers from 1 to 10
+
+let num = 1;
+while (num <= 10)
+{
+    console.log(num);
+    num +=2;
+}
+
+//count down from 5 to 1
+
+let a = 5;
+while (a>=1)
+{
+    console.log(a);
+    a--;
+}
+
+
+//print table of 2
+
+let c = 1;
+while (c <= 10)
+{
+    console.log("2 x" + c + "=" + (2*c));
+    c++;
+}
+
+
+//do while
+
+//
