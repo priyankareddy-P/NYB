4a07a8511e44456af54f935d97394cd11283bd0b
priyanka <priyanka.elluri@navayuvabharatinfotech.com>
2025-05-08 17:41:30 +0530

    map

--- javascript/25_03_25/parameters/map.js
+++ javascript/25_03_25/parameters/map.js
@@ -49,36 +49,41 @@ console.log(weakset.has(student));
 
 //add multiple objects to weakset
 
-let weakSet = new WeakSet();
-let car = {brand: "toyota"};
-let bike = {brand: "pulsor"};
-weakSet.add(car);
-weakset.add(bike);
-console.log(weakSet.has(car));
-console.log(weakSet.has(bike));
+// let weakSet = new WeakSet();
+// let car = {brand: "toyota"};
+// let bike = {brand: "pulsor"};
+// weakSet.add(car);
+// weakset.add(bike);
+// console.log(weakSet.has(car));
+// console.log(weakSet.has(bike));
 
 //weakmap
 //it works like a regular map, means keys must be objects(not strings or numbers)
 
-// let weakMap = new WeakMap();
-// let students = {name: "hima"};
-// weakMap.set(students, "Grade A");
-// console.log(weakMap.get(student));
+let weakMap = new WeakMap();
+let students = {name: "hima"};
+weakMap.set(students, "Grade A");
+console.log(weakMap.get(student));
 
 //using weakmap for hidden data
 
-let weakMap = new WeakMap();
-let person = {};
-function addSecret(person, secret)
-{
-    weakMap.set(person, secret);
-}
-function getSecret(person)
-{
-    return weakMap.get(person);
-}
-addSecret(person, "loves Pizza");
-console.log(getSecret(person));
+// let weakMap = new WeakMap();
+// let person = {};
+// function addSecret(person, secret)
+// {
+//     weakMap.set(person, secret);
+// }
+// function getSecret(person)
+// {
+//     return weakMap.get(person);
+// }
+// addSecret(person, "loves Pizza");
+// console.log(getSecret(person));
 
 
+//add an array to a weakset
 
+let weakSet = new weakSet();
+let myArray = [1,2,3];
+weakSet.add(myArray);
+console.log(weakSet.has(myArray));
